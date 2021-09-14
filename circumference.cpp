1c56413dd1479f8f769b17b819b0d5edfc4157f6
Daria Bernice <daria.bernice.calitis@mths.ca>
2021-09-14 17:44:07 +0000

    added circumference.cpp

---
+++ circumference.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2021 D.B. Calitis All rights reserved
+//
+// Created by: Daria Bernice Calitis
+// Created on: Sept 2021
+// This program calculates the circumference of a circle
+//    with the circumference that is given by the user
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    // this function calculates the circumference
+
+    const double TAU = M_PI * 2;
+    int radius;
+    double circumference;
+
+    // input
+    std::cout << "Enter the radius of the circle (cm): " << std::flush;
+    std::cin >> radius;
+
+    // process
+    circumference = TAU * radius;
+
+    // output
+    std::cout << "\nThe circumference is " <<
+    circumference << "cm" << std::endl;
+}
